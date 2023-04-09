4661aa531b9415165e1e12dd86163badf4936074
EsihleTyandela <esihletyandela@gmail.com>
2023-04-09 09:16:48 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * power - returns 2 ^ y
- * @c: only calculate if c != 0
- * @y: exponent value
- * Return: 2 ^ y or 0 if fail
- */
-int power(char c, int y)
-{
-	if (c == '0')
-		return (0);
-	else if (y == 0)
-		return (1);
-	else if (y == 1)
-		return (2);
-	return (2 * power(c, y - 1));
-}
-/**
- * binary - converts a string binary number into a unsigned int
- * @b: binary string
- * @num: pointer to number where value is stored
- * Return: decimal as unsigned int
- */
-unsigned int binary(const char *b, unsigned int *num)
-{
-	int index;
-
-	if (b[1] == '\0')
-	{
-		*num = b[0] - '0';
-		return (1);
-	}
-	index = binary(b + 1, num);
-	*num +=  power(b[0], index);
-	return (index + 1);
-}
-
-/**
- * binary_to_uint - converts a string binary number into a unsigned int
+ * binary_to_uint - converts a binary number to an int
  * @b: binary string
  * Return: decimal as unsigned int
  */
-unsigned int binary_to_uint(const char *b)
+
+unsigned int binary_to_unt(const char *b)
 {
-	unsigned int num, i;
+	unsigned int ret;
+	int p, q;
 
-	num = 0;
-
-	if (b == NULL || b[0] == '\0')
+	ret = 0;
+	p = 0;
+	q = 0;
+	if (b == NULL)
 		return (0);
-
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[p] != '\0')
+		p++;
+	p--;
+	while (p >= 0)
 	{
-		if (b[i] != '1' && b[i] != '0')
+		if (b[p] == '0' || b[p] == '1')
+		{
+			ret += (b[p] - '0') << q;
+			p--;
+			q++;
+		}
+		else
 			return (0);
 	}
-
-	binary(b, &num);
-	return (num);
+	return (ret);
 }
