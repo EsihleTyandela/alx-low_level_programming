6db6ca47fb1252de09b44e1ff5c5525eda2d5c7e
EsihleTyandela <esihletyandela@gmail.com>
2023-04-09 11:51:41 -0700

    Main file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,3 +11,5 @@ int get_endainness(void);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
