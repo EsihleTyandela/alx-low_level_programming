69e36a3245438bd320447f4b31a09fae74c56304
EsihleTyandela <esihletyandela@gmail.com>
2023-04-09 13:25:50 -0700

    Endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,13 +7,11 @@
 
 int get_endianness(void)
 {
-	int i;
-	char *s;
+	unsigned int x;
+	char *c;
 
-	i = 1;
-	s = (char *)&i;
-	if (s)
-		return (1);
-	else
-		return (0);
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
